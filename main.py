b24e5b56be97e150a2bdcf203d5ce708a477faf9
dimav <vesdim@gmail.com>
2024-09-29 20:54:57 +0300

    2

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 # Ввод данных от пользователя
-day = int(input("Введите день: "))
-month = int(input("Введите месяц: "))
-# Функция для определения сезона
-def determine_season(day, month):
-    if (month == 3 and day >= 1) or (month == 4) or (month == 5) or (month == 6 and day <= 20):
-        return "Весна"
-    elif (month == 6 and day >= 21) or (month == 7) or (month == 8) or (month == 9 and day <= 22):
-        return "Лето"
-    elif (month == 9 and day >= 23) or (month == 10) or (month == 11) or (month == 12 and day <= 20):
-        return "Осень"
-    else:
-        return "Зима"
-
-# Ввод данных от пользователя
-day = int(input("Введите день: "))
-month = int(input("Введите месяц: "))
-
-# Определение и вывод сезона
-season = determine_season(day, month)
-print(f"Время года: {season}")
+day = int(input("Введите день: ")) #Ввод дня
+month = int(input("Введите месяц: ")) #Ввод месяца
+#определение сезона
+if (month == 3 and day >= 1 and day <=31) or (month == 4 and day >= 1 and day <=30) or (month == 5 and day >= 1 and day <=31) :
+    season = "Весна" 
+elif (month == 6 and day >= 1) or (month == 7 and day >= 1 and day <=30) or (month == 8 and day >= 1 and day <=31) :
+    season = "Лето"
+elif (month == 9 and day >= 1 and day <=30) or (month == 10 and day >= 1 and day <=31 ) or (month == 11 and day >=1 and day <=30 ):
+    season = "Осень"
+elif (month == 1 and day >= 1 and day <=31) or (month == 2 and day >= 1 and day <=30 ) or (month == 12 and day >=1 and day <=28 ):
+    season = "Зима"
+else: 
+    season=" ERROR: Введены неккоректные данные"
+# вывод сезона
+print("Время года:", season)
