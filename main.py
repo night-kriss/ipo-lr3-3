b7c4bd496b9f9b265e0c40abf4bde14fea117d55
night-kriss <veseluhadima@gmail.com>
2024-10-03 09:16:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ month = int(input("Введите месяц: ")) #Ввод месяца
 #определение сезона
 if (month == 3 and day >= 1 and day <=31) or (month == 4 and day >= 1 and day <=30) or (month == 5 and day >= 1 and day <=31) :
     season = "Весна" 
-elif (month == 6 and day >= 1) or (month == 7 and day >= 1 and day <=30) or (month == 8 and day >= 1 and day <=31) :
+elif (month == 6 and day >= 1 and day <=30) or (month == 7 and day >= 1 and day <=31) or (month == 8 and day >= 1 and day <=31) :
     season = "Лето"
 elif (month == 9 and day >= 1 and day <=30) or (month == 10 and day >= 1 and day <=31 ) or (month == 11 and day >=1 and day <=30 ):
     season = "Осень"
-elif (month == 1 and day >= 1 and day <=31) or (month == 2 and day >= 1 and day <=30 ) or (month == 12 and day >=1 and day <=28 ):
+elif (month == 1 and day >= 1 and day <=31) or (month == 2 and day >= 1 and day <=29 ) or (month == 12 and day >=1 and day <=31 ):
     season = "Зима"
 else: 
     season=" ERROR: Введены неккоректные данные"
